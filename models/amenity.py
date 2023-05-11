e63f484cdd09f1308b87af7e19a3f977160bec0a
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-05-11 23:37:31 +0000

    adding User and more classes

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a module for Amenity class """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ represents a amenity of a city """
+    name = ""
